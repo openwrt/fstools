openwrt/fstools

commit 11efbf3b90f031ce634fc38cad07a4aef2985777

overlay: fix race condition when switching to jffs2
There is a race between `cp -a /tmp/root/* /rom/overlay` from
libfstools/overlay.c and a process creating new file(s) before
pivot(/rom, /mnt) occured.
That is a process can create a file and it will not be copied.

To workaround this, do additional copy after jffs2 is ready.
This doesn't completely solve the problem but since there
was no other fix provided since original RFC [1], it is better
than nothiing.

[1] https://www.mail-archive.com/openwrt-devel@lists.openwrt.org/msg38218.html

Signed-off-by: Roman Yeryomin <roman@advem.lv>

libfstools/overlay.c
@@ -243,6 +243,10 @@ jffs2_switch(struct volume *v)
 		ULOG_INFO("performing overlay whiteout\n");
 		umount2("/tmp/root", MNT_DETACH);
 		foreachdir("/overlay/", handle_whiteout);
+
+		/* try hard to be in sync */
+		ULOG_INFO("syncronizing overlay\n");
+		system("cp -a /tmp/root/upper/* / 2>/dev/null");
 		break;
 
 	case FS_EXT4:

commit 18090d97b61cc091ed3f49c3ec8ba42c761f1d15

overlay: fix compilation with glibc
glibc fails with:
warn_unused_result [-Wunused-result]
   system("cp -a /tmp/root/upper/* / 2>/dev/null");

Regression intoroduced with 11efbf3b90f031ce634fc38cad07a4aef2985777
Reported here:
https://www.mail-archive.com/lede-dev@lists.infradead.org/msg10502.html

Signed-off-by: Roman Yeryomin <roman@advem.lv>
Acked-by: Florian Fainelli <f.fainelli@gmail.com>

libfstools/overlay.c
@@ -257,7 +257,8 @@ jffs2_switch(struct volume *v)
 
 		/* try hard to be in sync */
 		ULOG_INFO("syncronizing overlay\n");
-		system("cp -a /tmp/root/upper/* / 2>/dev/null");
+		if (system("cp -a /tmp/root/upper/* / 2>/dev/null"))
+			ULOG_ERR("failed to sync jffs2 overlay\n");
 		break;
 
 	case FS_EXT4: