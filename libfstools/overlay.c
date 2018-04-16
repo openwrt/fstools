openwrt/fstools

commit cc63723d886fde2cd364a545b7cb05b70721b83f

overlay: use lstat rather than stat and make sure there are no trailing spaces
with this patch even uclibc wont deleted symlinked folders

Signed-off-by: John Crispin <blogic@openwrt.org>

libfstools/overlay.c
@@ -78,10 +78,16 @@ foreachdir(const char *dir, int (*cb)(const char*))
 		snprintf(globdir, 256, "%s/*", dir); /**/
 
 	if (!glob(globdir, GLOB_NOESCAPE | GLOB_MARK | GLOB_ONLYDIR, NULL, &gl))
-		for (j = 0; j < gl.gl_pathc; j++)
-			if (!stat(gl.gl_pathv[j], &s) && !S_ISLNK(s.st_mode))
-				foreachdir(gl.gl_pathv[j], cb);
+		for (j = 0; j < gl.gl_pathc; j++) {
+			char *dir = gl.gl_pathv[j];
+			int len = strlen(gl.gl_pathv[j]);
 
+			if (len > 1 && dir[len - 1] == '/')
+				dir[len - 1] = '\0';
+
+			if (!lstat(gl.gl_pathv[j], &s) && !S_ISLNK(s.st_mode))
+				foreachdir(gl.gl_pathv[j], cb);
+	}
 	cb(dir);
 }
 

commit 79721f0376974859f39699a2237dbfa691233523

libfstools: fix foreachdir() to pass dir with a trailing slash
Commit cc63723d886fd ("overlay: use lstat rather than stat and make sure
there are no trailing spaces") changed behavior of foreachdir() breaking
some callbacks. Before that modification all callbacks were getting
directory with a trailing slash. Above commit started removing them.

This broke handle_whiteout() which doesn't work at all since then. It
constructs file paths incorrectly: slash is missing between directory
and a file name. It seems noone noticed it for years because this issue
got hidden by switch2jffs() which also handles whiteouts with its system
command "cp -a" call.

Fix that regression by setting trailing slash back - right after calling
lstat(). Also to keep code simple just skip all entries that aren't
directories. This keeps conditions for removing/setting trailing slash
trivial. A side effect is not calling callbacks for files which is a
free bonus optimization.

Fixes: cc63723d886fd ("overlay: use lstat rather than stat and make sure there are no trailing spaces")
Signed-off-by: Rafał Miłecki <rafal@milecki.pl>
Acked-by: John Crispin <john@phrozen.org>

libfstools/overlay.c
@@ -88,15 +88,25 @@ foreachdir(const char *dir, int (*cb)(const char*))
 
 	sprintf(globdir, "%s/*", dir);
 
+	/* Include GLOB_MARK as callbacks expect a trailing slash */
 	if (!glob(globdir, GLOB_NOESCAPE | GLOB_MARK | GLOB_ONLYDIR, NULL, &gl))
 		for (j = 0; j < gl.gl_pathc; j++) {
 			char *dir = gl.gl_pathv[j];
 			int len = strlen(gl.gl_pathv[j]);
+			int err;
 
-			if (len > 1 && dir[len - 1] == '/')
+			/* Quick way of skipping files */
+			if (dir[len - 1] != '/')
+				continue;
+
+			/* lstat needs path without a trailing slash */
+			if (len > 1)
 				dir[len - 1] = '\0';
+			err = lstat(gl.gl_pathv[j], &s);
+			if (len > 1)
+				dir[len - 1] = '/';
 
-			if (!lstat(gl.gl_pathv[j], &s) && !S_ISLNK(s.st_mode))
+			if (!err && !S_ISLNK(s.st_mode))
 				foreachdir(gl.gl_pathv[j], cb);
 	}
 	cb(dir);