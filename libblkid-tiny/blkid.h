openwrt/fstools

commit b82c5c109c8545da1c4fea46e102aef25896c0e4

libblkid-tiny: add functions for allocating & freeing probe struct
This adds blkid_new_probe() and blkid_free_probe() which have to be used
in place of simple struct memory allocation. They will allow extending
probe struct by any extra initialization code and resources release.

Newly introduced probe.c file is based on original libblkid's code.

Signed-off-by: Rafał Miłecki <rafal@milecki.pl>

libblkid-tiny/libblkid-tiny.h
@@ -60,6 +60,9 @@ struct blkid_struct_probe
 	char	version[64];
 };
 
+struct blkid_struct_probe *blkid_new_probe(void);
+void blkid_free_probe(struct blkid_struct_probe *pr);
+
 extern int probe_block(char *block, struct blkid_struct_probe *pr);
 extern int mkblkdev(void);
 

libblkid-tiny/probe.c
@@ -1,0 +1,31 @@
+/*
+ * Low-level libblkid probing API
+ *
+ * Copyright (C) 2008-2009 Karel Zak <kzak@redhat.com>
+ *
+ * This file may be redistributed under the terms of the
+ * GNU Lesser General Public License.
+ */
+
+#include <stdlib.h>
+
+#include "libblkid-tiny.h"
+
+struct blkid_struct_probe *blkid_new_probe(void)
+{
+	struct blkid_struct_probe *pr;
+
+	pr = calloc(1, sizeof(struct blkid_struct_probe));
+	if (!pr)
+		return NULL;
+
+	return pr;
+}
+
+void blkid_free_probe(struct blkid_struct_probe *pr)
+{
+	if (!pr)
+		return;
+
+	free(pr);
+}

probe.c
@@ -21,33 +21,39 @@ static struct probe_info *
 probe_path_tiny(const char *path)
 {
 	struct probe_info *info = NULL;
-	struct blkid_struct_probe pr = { };
+	struct blkid_struct_probe *pr;
 	char *type, *dev, *uuid, *label, *version;
 
-	if (probe_block((char *)path, &pr) == 0 && pr.id && !pr.err) {
+	pr = blkid_new_probe();
+	if (!pr)
+		return NULL;
+
+	if (probe_block((char *)path, pr) == 0 && pr->id && !pr->err) {
 		info = calloc_a(sizeof(*info),
-		                &type,    strlen(pr.id->name) + 1,
-		                &dev,     strlen(pr.dev)      + 1,
-		                &uuid,    strlen(pr.uuid)     + 1,
-		                &label,   strlen(pr.label)    + 1,
-		                &version, strlen(pr.version)  + 1);
+		                &type,    strlen(pr->id->name) + 1,
+		                &dev,     strlen(pr->dev)      + 1,
+		                &uuid,    strlen(pr->uuid)     + 1,
+		                &label,   strlen(pr->label)    + 1,
+		                &version, strlen(pr->version)  + 1);
 
 		if (info) {
-			info->type = strcpy(type, pr.id->name);
+			info->type = strcpy(type, pr->id->name);
 
-			if (pr.dev[0])
-				info->dev = strcpy(dev, pr.dev);
+			if (pr->dev[0])
+				info->dev = strcpy(dev, pr->dev);
 
-			if (pr.uuid[0])
-				info->uuid = strcpy(uuid, pr.uuid);
+			if (pr->uuid[0])
+				info->uuid = strcpy(uuid, pr->uuid);
 
-			if (pr.label[0])
-				info->label = strcpy(label, pr.label);
+			if (pr->label[0])
+				info->label = strcpy(label, pr->label);
 
-			if (pr.version[0])
-				info->version = strcpy(version, pr.version);
+			if (pr->version[0])
+				info->version = strcpy(version, pr->version);
 		}
 	}
+
+	blkid_free_probe(pr);
 
 	return info;
 }

CMakeLists.txt
@@ -22,6 +22,7 @@ ADD_LIBRARY(blkid-tiny SHARED
 		libblkid-tiny/encode.c
 		libblkid-tiny/libblkid-tiny.c
 		libblkid-tiny/mkdev.c
+		libblkid-tiny/probe.c
 		libblkid-tiny/ext.c
 		libblkid-tiny/jffs2.c
 		libblkid-tiny/vfat.c

commit b4e25d54035c5092e8e9b46ab63500e95ac8c730

libblkid-tiny: fix symbol collision with full libblkid
The recent introduction of blkid_new_probe() and blkid_free_probe() in the
dynamically linked libblkid-tiny caused the dlopen'd libblkid.so to call into
the wrong version of blkid_new_probe() within blkid_new_probe_from_filename(),
leading to memory corruption and eventual segmentation faults.

Fixes: b82c5c1 ("libblkid-tiny: add functions for allocating & freeing probe struct")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

libblkid-tiny/blkid.h
@@ -215,11 +215,11 @@ extern char *blkid_evaluate_spec(const char *spec, blkid_cache *cache)
 			__ul_attribute__((warn_unused_result));
 
 /* probe.c */
-extern blkid_probe blkid_new_probe(void)
+extern blkid_probe blkidtiny_new_probe(void)
 			__ul_attribute__((warn_unused_result));
 extern blkid_probe blkid_new_probe_from_filename(const char *filename)
 			__ul_attribute__((warn_unused_result));
-extern void blkid_free_probe(blkid_probe pr);
+extern void blkidtiny_free_probe(blkid_probe pr);
 
 extern void blkid_reset_probe(blkid_probe pr);
 

libblkid-tiny/libblkid-tiny.h
@@ -62,8 +62,8 @@ struct blkid_struct_probe
 	struct list_head	buffers;	/* list of buffers */
 };
 
-struct blkid_struct_probe *blkid_new_probe(void);
-void blkid_free_probe(struct blkid_struct_probe *pr);
+struct blkid_struct_probe *blkidtiny_new_probe(void);
+void blkidtiny_free_probe(struct blkid_struct_probe *pr);
 
 extern int probe_block(char *block, struct blkid_struct_probe *pr);
 extern int mkblkdev(void);

libblkid-tiny/probe.c
@@ -14,7 +14,7 @@
 
 static int blkid_probe_reset_buffers(struct blkid_struct_probe *pr);
 
-struct blkid_struct_probe *blkid_new_probe(void)
+struct blkid_struct_probe *blkidtiny_new_probe(void)
 {
 	struct blkid_struct_probe *pr;
 
@@ -27,7 +27,7 @@ struct blkid_struct_probe *blkid_new_probe(void)
 	return pr;
 }
 
-void blkid_free_probe(struct blkid_struct_probe *pr)
+void blkidtiny_free_probe(struct blkid_struct_probe *pr)
 {
 	if (!pr)
 		return;

probe.c
@@ -24,7 +24,7 @@ probe_path_tiny(const char *path)
 	struct blkid_struct_probe *pr;
 	char *type, *dev, *uuid, *label, *version;
 
-	pr = blkid_new_probe();
+	pr = blkidtiny_new_probe();
 	if (!pr)
 		return NULL;
 
@@ -53,7 +53,7 @@ probe_path_tiny(const char *path)
 		}
 	}
 
-	blkid_free_probe(pr);
+	blkidtiny_free_probe(pr);
 
 	return info;
 }