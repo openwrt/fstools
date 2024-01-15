openwrt/fstools

commit 1a5695925ecf1f440fe30b7c2864facc5a297dce

mount_root: add support for passing args to mount_root start
It may be useful to pass args to mount_root start call. One usecase
might be pass additional configuration to enable special option on
mounting rootfs. Change main logic and permit additional args when start
is used.

No functional change intended as the default behaviour is saved by
continuing to call start if no args are provided.

Signed-off-by: Christian Marangi <ansuelsmth@gmail.com>

mount_root.c
@@ -119,7 +119,7 @@ done(int argc, char *argv[1])
 
 int main(int argc, char **argv)
 {
-	if (argc < 2)
+	if (argc < 2) || (!strcmp(argv[1], "start"))
 		return start(argc, argv);
 	if (!strcmp(argv[1], "ram"))
 		return ramoverlay();

commit 325d63d67006c5526ab9b0fbc62329da48aa93af

mount_root: fix compilation error for wrong condition
While reworking this to new implementation, a broken and old commit
slipped in by mistake.

Fix compilation error for wrong condition.

mount_root.c: In function 'main':
mount_root.c:131:23: error: expected expression before '||' token
  131 |         if (argc < 2) || (!strcmp(argv[1], "start"))
      |                       ^~
mount_root.c: At top level:
mount_root.c:31:1: error: 'start' defined but not used [-Werror=unused-function]
   31 | start(int argc, char *argv[3])
      | ^~~~~
cc1: all warnings being treated as errors

Fixes: 1a5695925ecf ("mount_root: add support for passing args to mount_root start")
Signed-off-by: Christian Marangi <ansuelsmth@gmail.com>

mount_root.c
@@ -128,7 +128,7 @@ done(int argc, char *argv[1])
 
 int main(int argc, char **argv)
 {
-	if (argc < 2) || (!strcmp(argv[1], "start"))
+	if (argc < 2 || !strcmp(argv[1], "start"))
 		return start(argc, argv);
 	if (!strcmp(argv[1], "ram"))
 		return ramoverlay();